05c000ac1b8fe036f961eae01b74172b4d6dac11
qtotuan <quynh.totuan@gmail.com>
2017-09-06 10:20:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 function findMinAndRemove(array){
-
+  let minValue = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minValue = array[i]
+      minIndex = i
+    }
+  }
+  return array.splice(minIndex, 1)
 }
 
-function insertionSort(array){
+// function insertionSort(array){
+//   // Loop through each element from left to right
+//   for (let i = 1; i < array.length; i++) {
+//     // While the element smaller is smaller than its left neighbor
+//     for (let j = i; j > 0; j--) {
+//       while (array[j] < array[j-1]) {
+//         // Swap the two elements
+//         let temp = array[j-1]
+//         array[j-1] = array[j]
+//         array[j] = temp
+//       }
+//     }
+//   }
+//   return array
+// }
 
+function insertionSort(array) {
+  let sorted = []
+  while (array.length> 0) {
+    sorted.push(findMinAndRemove(array)[0])
+  }
+  return sorted
 }
